d9235460fabd12aa71eb99d9838b7e369002ca99
Nicholus Somi <nicholaussomi5@gmail.com>
2021-10-29 00:57:57 +0300

    Our first commit

---
+++ src/main/java/com/example/demo/projct3SEPT/student/PageMappController.java
@@ -0,0 +1,24 @@
+package com.example.demo.projct3SEPT.student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageMappController {
+    @RequestMapping("")
+    public ModelAndView getLogin() {
+        return new ModelAndView("abouts");
+    }
+
+    @GetMapping("/about")
+    public String getAbout(){
+        return "aboutpg";
+    }
+    @GetMapping("/Team")
+    public String getTeam(){
+        return "team";
+    }
+}
+
---
+++ src/test/java/com/example/demo/projct3SEPT/MailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.projct3SEPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
